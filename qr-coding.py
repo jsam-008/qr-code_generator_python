96dfc6f53cb34fda3d04b6373692392ceb3892e8
sam j <jlatha541@gmail.com>
2025-02-04 19:58:33 +0530

    Update qr-coding.py

--- qr-coding.py
+++ qr-coding.py
@@ -1,9 +1,5 @@
 import qrcode
-
-# Ask the user to enter the text
 text = input("Enter the text to generate QR code: ")
-
-# Generate QR code
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -13,13 +9,10 @@ qr = qrcode.QRCode(
 qr.add_data(text)
 qr.make(fit=True)
 
-# Create an image from the QR Code instance
 img = qr.make_image(fill="black", back_color="white")
 
-# Save the QR code image
 img.save("qrcode.png")
 
-# Show the QR code
 img.show()
 
 print("QR Code generated and saved as 'qrcode.png'!")
